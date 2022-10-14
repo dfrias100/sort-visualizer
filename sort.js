e78e6acc591e10e35435073b347618be7abb4c09
Daniel Frias <dfrias41@gmail.com>
2022-10-14 03:16:39 -0500

    added gradients to bars

--- sort.js
+++ sort.js
@@ -4,7 +4,7 @@ function setup() {
     for (let i = 1; i <= 100; i++) {
         let sortBarDiv = document.createElement('div');
         sortBarDiv.className = "list-bar";
-        sortBarDiv.style.width = "1%";
+        sortBarDiv.style.width = "0.85%";
         sortBarDiv.style.height = i + "%";
         sortBarDiv.style.left = (i - 1) + "%";
         sortVizDiv.appendChild(sortBarDiv);
